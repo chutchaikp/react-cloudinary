c942808a2944e2e30a02db4b74bfa01b6aaafc7a
chutchai.kp <chutchai.kp@gmail.com>
2021-06-27 10:53:33 +0700

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Flex, Heading } from '@chakra-ui/react';
+import PhotoUploader from './components/PhotoUploader';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Flex justify="center" p="2rem" minH="100vh" >
+      <Flex direction="column" >
+        <Heading>Image gallery</Heading>
+        <PhotoUploader />
+      </Flex>
+    </Flex>
   );
 }
 
